0e36887ccf9ec9744622cd9ffdb1bb083ba204dc
yannisf <yannisf@ouranos>
2023-08-05 23:56:25 +0300

    Build scan

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,16 @@
 rootProject.name = "diceware"
+
+plugins {
+    id("com.gradle.enterprise") version("3.13.4")
+}
+
+gradleEnterprise {
+    if (System.getenv("CI") != null) {
+        buildScan {
+            publishAlways()
+            termsOfServiceUrl = "https://gradle.com/terms-of-service"
+            termsOfServiceAgree = "yes"
+        }
+    }
+}
+
